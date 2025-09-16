983a186daf50ab02223a3d4d5bd8ee553d92068b
Hemakeerthi M.D.M.H. <it24104168@my.sliit.lk>
2025-09-16 09:51:56 +0530

    Add IT24104168_Lab6 to Lab06

---
+++ Lab Sheet 06/Lab06.R
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\minur\\OneDrive\\Desktop\\IT24104168\\Lab06")
+
+#Part 01
+#Question 01
+#Binomial Distribution
+#Random variable X has binomial distribution with n=50 and p=0.85
+
+#question 02
+#It ask to find P(X>=47)
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#Part 02
+#Question 01
+#Number of calls received in an hour
+
+#Question 02
+#Poisson Distribution
+#Random variable X has poisson distribution with lambda=12
+
+#Question 03
+dpois(15,12)
+
